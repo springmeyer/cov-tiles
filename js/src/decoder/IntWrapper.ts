77d6b825d8d0bdbd4567db7a3ac6e7af41f5a3ca
springmeyer <dane.springmeyer@gmail.com>
2024-06-22 14:55:09 -0700

    allow direct access to value

--- js/src/decoder/IntWrapper.ts
+++ js/src/decoder/IntWrapper.ts
@@ -1,7 +1,7 @@
 // Ported from https://github.com/lemire/JavaFastPFOR/blob/master/src/main/java/me/lemire/integercompression/IntWrapper.java
 
 export class IntWrapper {
-    private value: number;
+    public value: number;
 
     constructor(private readonly v: number) {
         this.value = v;
